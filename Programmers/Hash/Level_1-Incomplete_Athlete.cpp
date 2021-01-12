97558e8707a3702c0227e8c8fe46cf3751168a91
Hyojin Choo <39365478+rainfall3363@users.noreply.github.com>
2021-01-12 17:41:54 +0900

    NOT pass efficiency test
    정확성 테스트 통과
    효율성 테스트 실패

---
+++ Programmers/Hash/Level_1-Incomplete_Athlete.cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(vector<string> participant, vector<string> completion) {
+    string answer = "";
+    bool* isParticipate = new bool[participant.size()];
+    for (int i = 0; i < participant.size(); i++) {
+        isParticipate[i] = false;
+    }
+    
+    // 동명이인 처리 방법?
+    for (int i = 0; i < completion.size(); i++) {
+        for (int j = 0; j < participant.size(); j++) {
+            if (!isParticipate[j] && !completion[i].compare(participant[j])) {
+                isParticipate[j] = true;
+                break;
+            }
+        }
+    }
+    
+    for (int i = 0; i < participant.size(); i++) {
+        if (!isParticipate[i]) {
+            answer += participant[i];
+        }
+    }
+
+    return answer;
+}
