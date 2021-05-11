dae160df0fa46d1d7f829ec6ad911d126331c75f
Hyojin Choo <39365478+rainfall3363@users.noreply.github.com>
2021-05-11 22:07:25 +0900

    greedy
    다른 사람의 풀이 1번 이해 안됨 다시 해보기

---
+++ Programmers/Greedy/Level_3-Making_Big_Number.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string number, int k) {
+    string answer = "";
+    int erased = 0;
+    // 내가 내 다음보다 작으면 나 삭제     
+    for (int i = 0; i < number.size() - 1; i++) {
+        if (erased == k) {
+            break;
+        }
+        if (number.at(i) < number.at(i + 1)) {   
+            number.erase(i, 1);
+            i = -1;
+            erased++;
+        }
+    }
+    // 이래도 안끝나면 뒤에서부터 삭제
+    for (int i = number.size() - 1; i >= 0; i--) {
+        if (erased == k) {
+            break;
+        }
+        number.erase(number.size() - 1, 1);
+        erased++;
+    }
+
+    answer += number;
+    return answer;
+}
